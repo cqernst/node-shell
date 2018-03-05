deca48d7fd21c5ead606fe10904751921c61f098
Annalee Switek <annaleejude@gmail.com>
2018-03-05 14:42:55 -0500

    up through curl

--- commands.js
+++ commands.js
@@ -1,7 +1,8 @@
 var fs = require('fs');
+var request = require('request');
 
 module.exports = {
-  pwd : function () {
+  pwd : function (file) {
           process.stdout.write(process.cwd());
         },
         // process.stdin.on('data', function (data) {
@@ -11,10 +12,10 @@ module.exports = {
         //   }
         // process.stdout.write('\nprompt > ');
         // }),
-  date : function () {
+  date : function (file) {
             process.stdout.write(Date(Date.now()).toString());
         },
-  ls : function () {
+  ls : function (file) {
         fs.readdir('.', function(err, files) {
             if (err) throw err;
             files.forEach(function(file) {
@@ -34,5 +35,14 @@ module.exports = {
         //   // process.stdout.write(text);
         // }
         process.stdout.write(text);
-      }
+      },
+  cat : function(file) {
+    fs.readFile(file, function(err, data){
+      console.log(data);
+    })
+  },
+
+  head : function() {
+    // prints the first 10 lines
+  }
 }
